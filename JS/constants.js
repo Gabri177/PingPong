5221a8490878fa6eeca6e43bd766af5827808ab1
Gabri177 <forestcat177@gmail.com>
2024-09-25 15:23:04 +0200

    delete 2d game

--- JS/constants.js
+++
@@ -1,6 +0,0 @@
-export const CANVAS_SCALE = 0.6;
-export const BALL_SPEED = 5;
-export const SCALE_BALL = 0.03;
-export const SCALE_PAD_WIDTH = 0.03;
-export const SCALE_PAD_HEIGHT = 0.2;
-export const FPS = 60;
--- JS/support.js
+++
@@ -1,6 +0,0 @@
-// output function to print the message to the screen
-export function output (tag, message) {
-
-	const output = document.getElementById(tag);
-	output.innerHTML = message + '<br>';
-}
